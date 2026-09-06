cd11ff3d02faa01562f6136a9542615cebe9f75b
ieasxu1407 <ieasxu1407@naver.com>
2026-09-06 14:10:20 +0900

    Update entrypoint.sh

--- copyables/entrypoint.sh
+++ copyables/entrypoint.sh
@@ -4,18 +4,22 @@ echo "Start entrypoint.sh"
 
 set -e
 
-
+# SSH host keys
 echo "Generating SSH host keys..."
 ssh-keygen -A
 
+# Password
 if [ -z "${USER_PASSWORD}" ]; then
-    echo "ERROR: USER_PASSWORD environment variable is not set."
+    echo "ERROR: USER_PASSWORD is not set."
     exit 1
 fi
 
 echo "Setting root password..."
 echo "root:${USER_PASSWORD}" | chpasswd
 
-echo "Starting SSH server..."
+# SSH runtime directory
+mkdir -p /tmp/sshd
 
-exec "$@"
+echo "Run sshd"
+
+exec "$@" -o PidFile=/tmp/sshd/sshd.pid
