56ac3ff6373d6dd0748837ff02400a482bf401eb
ieasxu1407 <ieasxu1407@naver.com>
2026-09-06 14:03:54 +0900

    Update entrypoint.sh

--- copyables/entrypoint.sh
+++ copyables/entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
+
 echo "Start entrypoint.sh"
 
 set -e
 
-# Folder for sshd. No Change.
 mkdir -p /var/run/sshd
 
-# Key generation
-ls /etc/ssh/ssh_host_* >/dev/null 2>&1 &&echo "Keys is found" ||echo "Key generation." && ssh-keygen -A
+echo "Generating SSH host keys..."
+ssh-keygen -A
 
-# Environment variables that are used if not empty:
-# USER_PASSWORD
-
-#Set password
-if [ -f /.ispasswordset ]; then
-    echo "Password already set"
-else
-    echo "Set  password of user for sshd"
-    echo 'root:'${USER_PASSWORD} |chpasswd
-    touch /.ispasswordset
+if [ -z "${USER_PASSWORD}" ]; then
+    echo "ERROR: USER_PASSWORD environment variable is not set."
+    exit 1
 fi
 
-echo "Run sshd"
+echo "Setting root password..."
+echo "root:${USER_PASSWORD}" | chpasswd
+
+echo "Starting SSH server..."
 
 exec "$@"
